7201dd58bc08a2fc0efe198e75ef574d6656074b
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-02-25 21:55:49 +0000

    Modifications to ReadMe plugin to facilitate JS TOC creation, submenu addition, sidebar menu placement in WP2.7 (including MU), removed redundant function_exists call wrapping a require_once, made user_level a filterable value. Should not break backwards compatibility.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-readme/trunk@8349 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-readme.php
+++ cf-readme.php
@@ -13,111 +13,251 @@ Author URI: http://crowdfavorite.com
  * 1. Uses the cf-compat plugin for < WP 2.6 compatability
  * 2. Uses the cf-compat plugin for < PHP 4.3 compatability (for file_get_contents)
  * 3. Requires the cf-compat plugin for is_admin_page function
- * 4. Plugin adds a dashboard sub-menu in WPMU due to bugs in MU's implementation of add_menu_page
- * 5. Set content using the cfreadme_content filter
+ * 4. Plugin adds a dashboard sub-menu in WPMU < 2.7 due to bugs in MU's implementation of add_menu_page
+ * 5. Set content using the cfreadme_content filter, there is currently no queue for setting dislay order of added items
+ * 6. Plugin automatically breaks content sections at H2 elements and creates a TOC based on the H2 content
+ * 7. Sections can be linked in to by using /wp-admin/index.php?page=cf-faq#readme-id-of-element where the id of the section to be targeted is "id-of-element"
+ *	  Section IDs are generated automatically unless an anchor (<a>) is found as a child of the H2 and that anchor has an ID
+ *	  The section ID can be found in the TOC list at the top - to link in from another page just add "readme-" to the front of it to trigger the switch at page load
+ * 8. Additional ReadMe pages can be added under the FAQ Menu. See the bottom of this file for example code
  */
 
-if (!function_exists('is_admin_page')) {
-	function is_admin_page() {
-		if (function_exists('is_admin')) {
-			return is_admin();
+// ADMIN MENU ITEMS
+
+	/**
+	 * Add top level admin menu item
+	 * Top level menu items are a bit convoluted, we need the plugin to only
+	 * add the menu item and point to a completely separate functionality page
+	 *
+	 * User level works off of the older user level integers that can be 
+	 * passed to add_(sub)menu_page funcitons to designate the user access 
+	 * level of the plugin page
+	 */
+	function cfreadme_menu_items() {
+		global $wpmu_version, $wp_version;
+		$user_level = apply_filters('cfreadme_user_level',2);
+	
+		// add submenu to dashboard
+		if (is_admin_page()) {
+			if (is_null($wpmu_version) || version_compare($wpmu_version,'2.7','>=')) {
+				add_menu_page('faq','FAQ',$user_level,'cf-faq','cfreadme_show');
+				if(version_compare($wpmu_version,'2.7','>=')) {
+					add_action('admin_init','cfreadme_sort_admin_menu',999);
+				}
+			}
+			else {
+				// wpmu hack for top level menu items, don't like it, nope, not one bit
+				add_submenu_page('index.php','faq','FAQ',$user_level,'cf-faq','cfreadme_show');
+			}
+			// load js
+			if(is_plugin_page()) {
+				wp_enqueue_script('cfreadme-js',trailingslashit(get_bloginfo('url')).'index.php?cf_action=cfreadme_js','jquery','1.0');
+			}
 		}
-		if (function_exists('check_admin_referer')) {
-			return true;
+	}
+	add_action('admin_menu', 'cfreadme_menu_items');
+
+	/**
+	 * Sort the admin menu items to force our menu item to be first
+	 */
+	function cfreadme_sort_admin_menu() {
+		global $menu;
+	
+		$menu_sep = $dash = null;
+		foreach($menu as $key => $menu_item) {
+			// grab the dashboard item, find it explicitly in case anyone else has moved it
+			if(isset($menu_item[5]) && $menu_item[5] == 'menu-dashboard' && $dash == null) {
+				$dash = $menu_item;
+				unset($menu[$key]);
+			}
+			// we'll most certainly hit a separator before we hit our menu, clone it
+			if($menu_item[4] == 'wp-menu-separator' && $menu_sep == null) {
+				$menu_sep = $menu_item;
+			}
+			// unset the current FAQ position and shove it and a separator on the front of the menu
+			if($menu_item[0] == 'FAQ') {
+				unset($menu[$key]);
+				array_unshift($menu,$dash,$menu_sep,$menu_item);
+			}
 		}
-		else {
+	}
+
+
+// DISPLAY CONTENT
+
+	/**
+	 * Show appropriate FAQ page
+	 */
+	function cfreadme_show() {
+		if (!function_exists('Markdown')) {
+			require_once(realpath(dirname(__FILE__)).'/markdown/markdown.php');
+		}
+		$content = apply_filters('cfreadme_content', 'No content set.');
+		$html = Markdown($content);
+		// modify content to facilitate the creation of a JavaScript TOC
+		$html = preg_replace("/(<\/h2>)(.*?)(<h2>|$)/si","$1<div>$2</div>$3",$html);
+		echo '
+	<div class="wrap cf-readme">
+		'.$html.'
+	</div>
+		';	
+	}
+
+
+// CSS
+
+	function cfreadme_css() {
+		echo "
+		<style type='text/css'>
+			.cf-readme ul, 
+			.cf-readme ol {
+				margin: 0 0 1em 1.5em;
+			}
+			.cf-readme dd {
+				margin-left:1.5em;
+			}
+			.cf-readme ul {
+				list-style-type: disc;
+			}
+			.cf-readme li {
+				margin-bottom:0;
+			}
+			.cf-readme li ul,
+			.cf-readme li ol {
+				margin-bottom:0;
+			}
+			.cf-readme li ul {
+				list-style-type: circle;
+			}
+			.cf-readme li li ul {
+				list-style-type: square;
+			}
+			.cf-readme ol {
+				list-style-type: decimal;
+			}
+			.cf-readme pre {
+				background:#eaeaea;
+				border:.5em solid #eaeaea;
+				margin-bottom:1em;
+				overflow:auto;
+			}
+			.cf-readme blockquote {
+				border-left:.25em solid #eaeaea;
+				margin-left:0;
+				padding-left:.75em;
+			}
+		</style>
+		";
+	}
+	add_action('admin_head', 'cfreadme_css');
+
+
+// JAVASCRIPT
+
+	/**
+	 * Javascript for organizing the UI in to tabs based on the H2 tag
+	 */
+	function cfreadme_javascript() {
+		echo '
+<script type="text/javascript">
+//<[CDATA[
+	// find all H2s and make tab sets
+	jQuery(function(){
+		// make tab ul
+		jQuery("<ul>").attr("id","readme-tabs").insertAfter(jQuery(".cf-readme h1"));
+
+		var divcount = 0;
+		jQuery(".cf-readme h2").each(function(){
+			_this = jQuery(this);
+			divcount++;
+
+			// make sure the h2 does not contain a link, if so, use the link text
+			if(_this.children("a").length) {
+				child = _this.children("a");
+				link_text = child.html();
+				if(child.attr("id").length) {
+					div_id = child.attr("id");
+					child.removeAttr("id");
+				}
+				else {
+					div_id = "section-" + divcount;
+				}
+			}
+			else {
+				link_text = _this.html();
+				div_id = "section-" + divcount;
+			}
+
+			// add an li to the tab list
+			jQuery("<li>").append(
+				jQuery("<a>").attr("href","#"+div_id).html(link_text)
+			).appendTo(jQuery("#readme-tabs"));
+			// give the trailing div an id and move the h2 inside
+			_this.next("div").attr("id",div_id).prepend(_this);
+		});
+
+		// make the tab list do neat stuff
+		jQuery("#readme-tabs li:first-child").addClass("active");
+		jQuery("#readme-tabs li a").each(function(){
+			_this = jQuery(this);
+			if(_this.parent().attr("class") != "active") {
+				jQuery(_this.attr("href")).hide();
+			}
+		}).click(function(){
+			_this = jQuery(this);
+			jQuery(_this.attr("href")).show().siblings("div").hide();
+			_this.parent().addClass("active").siblings().removeClass("active");
 			return false;
+		});
+
+		// @TODO - trigger click on tab link if hash present
+		view_section = location.hash.replace("#readme-","");
+		jQuery("#readme-tabs li a[href=\"#"+view_section+"\"]").click();
+	});
+//]]>
+</script>
+		';
+	}
+	add_action('admin_head','cfreadme_javascript');
+	
+
+// COMPATABILITY
+
+	if (!function_exists('is_admin_page')) {
+		function is_admin_page() {
+			if (function_exists('is_admin')) {
+				return is_admin();
+			}
+			if (function_exists('check_admin_referer')) {
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
 	}
-}
+	
+	
+// ADDITIONAL FAQ PAGE SAMPLE CODE
 
-function cfreadme_css() {
-	echo "
-	<style type='text/css'>
-		.cf-readme ul, 
-		.cf-readme ol {
-			margin: 0 0 1em 1.5em;
-		}
-		.cf-readme dd {
-			margin-left:1.5em;
-		}
-		.cf-readme ul {
-			list-style-type: disc;
-		}
-		.cf-readme li {
-			margin-bottom:0;
-		}
-		.cf-readme li ul,
-		.cf-readme li ol {
-			margin-bottom:0;
-		}
-		.cf-readme li ul {
-			list-style-type: circle;
-		}
-		.cf-readme li li ul {
-			list-style-type: square;
-		}
-		.cf-readme ol {
-			list-style-type: decimal;
-		}
-		.cf-readme pre {
-			background:#eaeaea;
-			border:.5em solid #eaeaea;
-			margin-bottom:1em;
-			overflow:auto;
-		}
-		.cf-readme blockquote {
-			border-left:.25em solid #eaeaea;
-			margin-left:0;
-			padding-left:.75em;
-		}
-	</style>
-	";
-}
-add_action('admin_head', 'cfreadme_css');
-
-/**
- * User level
- * works off of the older user level integers that can be 
- * passed to add_(sub)menu_page funcitons to designate the
- * user access level of the plugin page
- */
-$user_level = 2;
-
-/**
- * Add top level admin menu item
- * Top level menu items are a bit convoluted, we neced the plugin to only
- * add the menu item and point to a completely separate functionality page
- */
-function cfreadme_menu_items() {
-	global $wpmu_version, $user_level;
-	// add submenu to dashboard
-	if (is_admin_page()) {
-		if (is_null($wpmu_version)) {
-			add_menu_page('faq','FAQ',$user_level,'cf-faq','cfreadme_show');
-		}
-		else {
-			// wpmu hack for top level menu items, don't like it, nope, not one bit
-			//add_menu_page('faq','FAQ',$user_level,'admin.php?page=cf-readme/cf-readme.php','show_readme'); // this doesn't call the function 'show_readme'
-			add_submenu_page('index.php','faq','FAQ',$user_level,'cf-faq','cfreadme_show');
-		}
+	/**
+	 * example of adding another admin menu page using the same plugin code
+	 * @NOTE - add_submenu_page must have a specific identifier for the 1st item
+	 *		add_submenu_page('cf-faq',$your_faq_id,$your_faq_title,$user_access_level,$your_faq_page_name,'cfreadme_show');
+	 */
+	/*
+	function add_another_readme_menu() {
+		global $user_level;
+		add_submenu_page('cf-faq','my-faq','My FAQ',$user_level,'my-cf-faq','cfreadme_show');
+		add_action('cfreadme_content','another_readme_page_content');
 	}
-}
-add_action('admin_menu', 'cfreadme_menu_items');
+	add_action('admin_menu','add_another_readme_menu',999);
 
-/**
- * Show appropriate FAQ page
- */
-function cfreadme_show() {
-	if (!function_exists('Markdown')) {
-		require_once(realpath(dirname(__FILE__)).'/markdown/markdown.php');
+	function another_readme_page_content($readme_content) {
+		if($_GET['page'] == 'my-cf-faq') {
+			$readme_content = file_get_contents('my-faq.txt');
+		}
+		return $readme_content;
 	}
-	$content = apply_filters('cfreadme_content', 'No content set.');
-	echo '
-<div class="wrap cf-readme">
-	'.Markdown($content).'
-</div>
-	';	
-}
-
+	*/
 ?>
